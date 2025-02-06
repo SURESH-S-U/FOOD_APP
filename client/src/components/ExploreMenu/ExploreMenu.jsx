819cf61d6ea03863c9a7f8ad21d5a48e2efe1a06
SURESH-S-U <suresh.elite07@gmail.com>
2025-02-06 21:05:43 +0530

    Explore Menu Component Added

--- client/src/components/ExploreMenu/ExploreMenu.jsx
+++ client/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExploreMenu.css'
 
 import { menu_list } from '../../assets/frontend_assets/assets'
 
 const ExploreMenu = () => {
+
+  const [category,setCategory] = useState("All");
+
+  
   return (
     <div className='explore-menu'  id='explore-menu'>
-        <h1>Elplore our Menu</h1>
+        <h1>Explore our Menu</h1>
         <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eveniet nulla voluptate consectetur vero laudantium, provident eos numquam labore corrupti nam qui? Soluta porro at nobis quas magni, aliquam nemo?</p>
         <div className="explore-menu-list">
             {menu_list.map( (item) => {
               return(
-                <div className="explore-menu-list-items">
-                  <img src={item.menu_image} alt="" />
+                <div onClick={() => setCategory(prev=>prev===item.menu_name?"All":item.menu_name)}  className="explore-menu-list-items">
+                  <img className = {category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                   <p>{item.menu_name}</p>
                 </div>
               )
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+
   return (
     <div className='home'>
         <p>Home</p>
 
         <Header/>
-        <ExploreMenu/>
+        <ExploreMenu />
 
     </div>
   )
